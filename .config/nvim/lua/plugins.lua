41b7ecb28f8757c54f1634b4ccf6f17edd75a70d
Piory <aiueo8102@gmail.com>
2025-05-26 07:07:17 +0900

    nvim に noice.nvim を追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,11 +44,11 @@ return require('lazy').setup({
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     }
-  },  
+  },
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 
+    dependencies = {
       'nvim-lua/plenary.nvim',
     }
   },
@@ -56,5 +56,19 @@ return require('lazy').setup({
     'shellRaining/hlchunk.nvim',
     event = { "BufReadPre", "BufNewFile" },
   },
-  
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
 })
