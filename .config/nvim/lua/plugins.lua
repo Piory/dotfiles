e34f528e9db113ff0cc3f6564ccc683c035dcb9b
Piory <aiueo8102@gmail.com>
2025-05-26 16:55:43 +0900

    nvim に folke/which-key.nvim を追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -26,6 +26,24 @@ return require('lazy').setup({
     }
   },
   {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show({ global = false })
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
+  },
+  {
   	'lukas-reineke/indent-blankline.nvim',
   	main = "ibl",
   	opts = function(_, opts)
