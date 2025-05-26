22a99b5c9f862a8d2e5615f0108c4c1e23f069af
Piory <aiueo8102@gmail.com>
2025-05-26 22:29:39 +0900

    no-neck-pain 一旦無効

--- .config/nvim/lua/setup/no-neck-pain.lua
+++ .config/nvim/lua/setup/no-neck-pain.lua
@@ -1,12 +1,18 @@
-require('no-neck-pain').setup({
-  width = 200,
-  buffers = {
-    blend = 0.3,
-  },
-	autocmds = {
-		enableOnVimEnter = true,
-		enableOnTabEnter = true,
-		reloadOnColorSchemeChange = true,
-	},
-})
-vim.keymap.set('n', '<leader>z', '<cmd>NoNeckPain<CR>')
+-- require('no-neck-pain').setup({
+--   width = 200,
+--   buffers = {
+--     blend = 0.3,
+--     right = {
+--       enabled = false,
+--     },
+--     left = {
+--       enabled = false,
+--     },
+--   },
+--   autocmds = {
+--     enableOnVimEnter = true,
+--     enableOnTabEnter = true,
+--     reloadOnColorSchemeChange = true,
+--   },
+-- })
+-- vim.keymap.set('n', '<leader>z', '<cmd>NoNeckPain<CR>')
