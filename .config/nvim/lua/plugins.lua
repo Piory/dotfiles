218723dfe3f315a00d2e1dd6bc1b9d1c3fa41696
Piory <aiueo8102@gmail.com>
2025-05-26 15:20:34 +0900

    nvim に auto-session を追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,6 +15,17 @@ return require('lazy').setup({
   'brenoprata10/nvim-highlight-colors',
   'mvllow/modes.nvim',
   {
+    'rmagatti/auto-session',
+    lazy = false,
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    }
+  },
+  {
   	'lukas-reineke/indent-blankline.nvim',
   	main = "ibl",
   	opts = function(_, opts)
