4d5fbf36dd2b5fc08e79ee140878ac8da2fc9cc3
Piory <aiueo8102@gmail.com>
2025-05-26 15:34:10 +0900

    nvim の hlchunk の色変更

--- .config/nvim/lua/setup/hlchunk.lua
+++ .config/nvim/lua/setup/hlchunk.lua
@@ -1,6 +1,11 @@
 require('hlchunk').setup({
   chunk = {
-    enable = true
+    enable = true,
+    style = '#1abc9c',
+  },
+  line_num = {
+    enable = true,
+    style = '#1abc9c',
   },
   indent = {
     enable = true
