672697bd8ac2955bda011f179acd1f59048692c8
Piory <aiueo8102@gmail.com>
2025-05-27 15:56:08 +0900

    LSP 関連のプラグインを plugins/lsp.lua に移動

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,8 @@
 return {
+  'nvim-lua/lsp-status.nvim',
+  'onsails/lspkind.nvim',
+  'neovim/nvim-lspconfig',
+  'arkav/lualine-lsp-progress',
   {
     'ray-x/lsp_signature.nvim',
     event = 'InsertEnter',
@@ -7,6 +11,56 @@ return {
       handler_opts = {
         border = 'rounded'
       },
+    },
+  },
+  {
+    'mason-org/mason.nvim',
+    build = ':MasonUpdate',
+    cmd = { 'Mason', 'MasonUpdate', 'MasonLog', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll' },
+    config = true,
+  },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    dependencies = {
+      { 'mason-org/mason.nvim' },
+      { 'neovim/nvim-lspconfig' },
+    },
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = true,
+    keys = {
+      { '<C-space>', '<cmd>lua vim.lsp.completion.get()  <CR>', mode = 'i' },
+      { 'gh',        '<cmd>lua vim.lsp.buf.hover()       <CR>' },
+      { 'gd',        '<cmd>lua vim.lsp.buf.definition()  <CR>' },
+      { 'gD',        '<cmd>lua vim.lsp.buf.declaration() <CR>' },
+    },
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require('lspsaga').setup({
+        finder = {
+          max_height = 0.6,
+          default = 'tyd+ref+imp+def',
+          keys = {
+            toggle_or_open = '<CR>',
+            vsplit = 'v',
+            split = 's',
+            tabnew = 't',
+            tab = 'T',
+            quit = 'q',
+            close = '<Esc>',
+          },
+          methods = {
+            tyd = 'textDocument/typeDefinition',
+          }
+        }
+      })
+
+      vim.keymap.set('n', '<leader>,', '<Cmd>Lspsaga finder<CR>', { desc = 'Telescope: live grep args', })
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
     },
   },
 }
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,4 @@
 return {
-  'nvim-lua/lsp-status.nvim',
-  'onsails/lspkind.nvim',
   'gen740/SmoothCursor.nvim',
   'hrsh7th/cmp-cmdline',
   'hrsh7th/cmp-buffer',
@@ -13,7 +11,6 @@ return {
   'mvllow/modes.nvim',
   'kevinhwang91/nvim-hlslens',
   'kazhala/close-buffers.nvim',
-  'arkav/lualine-lsp-progress',
   {
     'goolord/alpha-nvim',
     -- dependencies = { 'echasnovski/mini.icons' },
@@ -76,59 +73,6 @@ return {
   {
     'github/copilot.vim',
     lazy = false,
-  },
-  {
-    'neovim/nvim-lspconfig',
-  },
-  {
-    'mason-org/mason.nvim',
-    build = ':MasonUpdate',
-    cmd = { 'Mason', 'MasonUpdate', 'MasonLog', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll' },
-    config = true,
-  },
-  {
-    'mason-org/mason-lspconfig.nvim',
-    dependencies = {
-      { 'mason-org/mason.nvim' },
-      { 'neovim/nvim-lspconfig' },
-    },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = true,
-    keys = {
-      { '<C-space>', '<cmd>lua vim.lsp.completion.get()  <CR>', mode = 'i' },
-      { 'gh',        '<cmd>lua vim.lsp.buf.hover()       <CR>' },
-      { 'gd',        '<cmd>lua vim.lsp.buf.definition()  <CR>' },
-      { 'gD',        '<cmd>lua vim.lsp.buf.declaration() <CR>' },
-    },
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-      require('lspsaga').setup({
-        finder = {
-          max_height = 0.6,
-          default = 'tyd+ref+imp+def',
-          keys = {
-            toggle_or_open = '<CR>',
-            vsplit = 'v',
-            split = 's',
-            tabnew = 't',
-            tab = 'T',
-            quit = 'q',
-            close = '<Esc>',
-          },
-          methods = {
-            tyd = 'textDocument/typeDefinition',
-          }
-        }
-      })
-
-      vim.keymap.set('n', '<leader>,', '<Cmd>Lspsaga finder<CR>', { desc = 'Telescope: live grep args', })
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-    },
   },
   {
     'akinsho/flutter-tools.nvim',
