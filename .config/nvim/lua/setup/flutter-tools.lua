866bcf71dc470caaa4edf0dbb14796e0cc97ea8c
Piory <aiueo8102@gmail.com>
2025-05-27 21:15:35 +0900

    flutter-tools のプロジェクト設定

--- .config/nvim/lua/setup/flutter-tools.lua
+++ .config/nvim/lua/setup/flutter-tools.lua
@@ -1,1 +1,22 @@
-require('flutter-tools').setup({})
+local flutter_tools = require('flutter-tools')
+flutter_tools.setup({})
+flutter_tools.setup_project({
+  {
+    name = 'Localhost',
+    target = 'lib/main.dart',
+    cwd = 'apps/r4b_app',
+    dart_define_from_file = 'dart_defines/local.json',
+  },
+  {
+    name = 'Development',
+    target = 'lib/main.dart',
+    cwd = 'apps/r4b_app',
+    dart_define_from_file = 'dart_defines/dev-development.json',
+  },
+  {
+    name = 'Production',
+    target = 'lib/main.dart',
+    cwd = 'apps/r4b_app',
+    dart_define_from_file = 'dart_defines/prd-production.json',
+  },
+})
