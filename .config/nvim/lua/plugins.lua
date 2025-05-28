b2e3c6b4d2bae43caa12fb598d8d0c7559ecddad
Piory <aiueo8102@gmail.com>
2025-05-28 18:03:57 +0900

    nvim に numToStr/Comment.nvim, JoosepAlviste/nvim-ts-context-commentstring を追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,9 +1,10 @@
 require('lazy').setup({
   spec = {
     { import = 'plugins/ai' },
+    { import = 'plugins/comment' },
     { import = 'plugins/flash' },
     { import = 'plugins/lsp' },
     { import = 'plugins/oil' },
     { import = 'plugins/plugins' },
-  }
+  },
 })
---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
+  'JoosepAlviste/nvim-ts-context-commentstring',
+}
