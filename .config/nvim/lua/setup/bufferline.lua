58344efe7e7fd8556b0eef03d3152be582649faf
Piory <aiueo8102@gmail.com>
2025-05-28 19:00:00 +0900

    bufferline の設定追加

--- .config/nvim/lua/setup/bufferline.lua
+++ .config/nvim/lua/setup/bufferline.lua
@@ -1,3 +1,17 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
-
+local bufferline = require('bufferline')
+bufferline.setup({
+  options = {
+    separator_style = 'slant',
+    diagnostics = 'nvim_lsp',
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = ' '
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == 'error' and ' '
+            or (e == 'warning' and ' ' or ' ')
+        s = s .. n .. sym
+      end
+      return s
+    end,
+  },
+})
