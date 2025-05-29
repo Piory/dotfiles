87b44e6deebceff79afc21c039d184949d288aab
Piory <aiueo8102@gmail.com>
2025-05-30 00:30:20 +0900

    nvim に Pocco81/auto-save.nvim を追加

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -154,6 +154,15 @@ return {
     },
   },
   {
+    'Pocco81/auto-save.nvim',
+    config = function()
+      require('auto-save').setup({
+        -- your config goes here
+        -- or just leave it empty :)
+      })
+    end,
+  },
+  {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
