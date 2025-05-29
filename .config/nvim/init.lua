ebca29d310b5f09ade06c9ad95fa77693e197776
Piory <aiueo8102@gmail.com>
2025-05-29 18:39:49 +0900

    DiagnosticSign のフォント変更

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,6 +47,13 @@ require('setup.bufferline')
 require('setup.smooth-cursor')
 require('setup.modes')
 
+-- ステータウスラインの設定
+local signs = { Error = ' ', Warn = ' ', Hint = '󱩎 ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl })
+end
+
 -- 背景透明化の設定
 vim.cmd([[
   highlight Normal guibg=none
