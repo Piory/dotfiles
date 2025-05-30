5472cc0a9c038101a56cafe59f0e6c208d52aab6
Piory <aiueo8102@gmail.com>
2025-05-30 09:10:51 +0900

    keymaps.lua フォーマット

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,23 +1,36 @@
 local keymap = vim.keymap
-vim.g.mapleader =
-' '                                                                                                   -- <leader> を半角スペースに設定
+vim.g.mapleader = ' '                             -- <leader> を半角スペースに設定
 
-keymap.set('i', 'jj', '<ESC>', { silent = true })                                                     -- インサートモードを抜ける
-keymap.set('n', ';', ':', { silent = true })                                                          -- シフトを押すことなく、;を入力する。割と便利
+keymap.set('i', 'jj', '<ESC>', { silent = true }) -- インサートモードを抜ける
+keymap.set('n', ';', ':', { silent = true })      -- シフトを押すことなく、;を入力する。割と便利
 
-keymap.set('n', '<leader>w', ':w<CR>')                                                                -- 保存
-keymap.set('n', '<leader>q', ':q<CR>')                                                                -- 終了
-keymap.set('n', '<leader>c', ':nohlsearch<CR>')                                                       -- 検索のハイライトを消す
-keymap.set('n', '<leader>sv', '<C-w>v')                                                               -- ウィンドウを垂直方向に分割する
-keymap.set('n', '<leader>sh', '<C-w>s')                                                               -- ウィンドウを水平に分割する
-keymap.set('n', '<leader>se', '<C-w>=')                                                               -- ウィンドウの幅を等分にする
-keymap.set('n', '<leader>sx', ':close<CR>')                                                           -- 現在、カーソルがいるウィンドウを閉じる
-keymap.set('n', '<leader>sw', '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>')                     -- ウィンドウの幅を少し広げる
-keymap.set('n', '<leader>sww',
-  '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>') -- ウィンドウの幅を大きく広げる
-keymap.set('n', '<leader>st', '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><')                     -- ウィンドウの幅を少し狭める
-keymap.set('n', '<leader>stt',
-  '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><') -- ウィンドウの幅を大きく狭める
+keymap.set('n', '<leader>w', ':w<CR>')            -- 保存
+keymap.set('n', '<leader>q', ':q<CR>')            -- 終了
+keymap.set('n', '<leader>c', ':nohlsearch<CR>')   -- 検索のハイライトを消す
+keymap.set('n', '<leader>sv', '<C-w>v')           -- ウィンドウを垂直方向に分割する
+keymap.set('n', '<leader>sh', '<C-w>s')           -- ウィンドウを水平に分割する
+keymap.set('n', '<leader>se', '<C-w>=')           -- ウィンドウの幅を等分にする
+keymap.set('n', '<leader>sx', ':close<CR>')       -- 現在、カーソルがいるウィンドウを閉じる
+keymap.set(
+  'n',
+  '<leader>sw',
+  '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>'
+) -- ウィンドウの幅を少し広げる
+keymap.set(
+  'n',
+  '<leader>sww',
+  '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>'
+) -- ウィンドウの幅を大きく広げる
+keymap.set(
+  'n',
+  '<leader>st',
+  '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><'
+) -- ウィンドウの幅を少し狭める
+keymap.set(
+  'n',
+  '<leader>stt',
+  '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><'
+) -- ウィンドウの幅を大きく狭める
 
 -- タブを作成、削除
 keymap.set('n', '<leader>to', ':tabnew<CR>')
@@ -35,16 +48,41 @@ keymap.set('n', '<leader>aW', "ciw''<Esc>P")
 -- タブの移動
 keymap.set('n', 'gt', ':bnext<CR>')
 keymap.set('n', 'gT', ':bprev<CR>')
+
+-- LSP handlers
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  { virtual_text = true }
+)
+
 -- nvim-tree のトグル
-vim.api.nvim_set_keymap('n', '<leader>s', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>s',
+  ':NvimTreeToggle<CR>',
+  { noremap = true, silent = true }
+)
 -- Toggleterm
 local builtin = require('telescope.builtin')
-keymap.set('n', '<leader>sf', builtin.find_files, {})
-keymap.set('n', '<leader>sg', builtin.live_grep, {})
-keymap.set('n', '<leader>sb', builtin.buffers, {})
-keymap.set('n', '<leader>sh', builtin.help_tags, {})
+keymap.set('n', '<leader>sf', function()
+  builtin.find_files()
+end)
+keymap.set('n', '<leader>sg', function()
+  builtin.live_grep()
+end)
+keymap.set('n', '<leader>sb', function()
+  builtin.buffers()
+end)
+keymap.set('n', '<leader>sh', function()
+  builtin.help_tags()
+end)
 -- BDelete
-vim.api.nvim_set_keymap('n', '<leader>x', ':BDelete this<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-x>',
+  ':BDelete this<CR>',
+  { noremap = true, silent = true }
+)
 -- vim.api.nvim_set_keymap('n', '<leader>xo', ':BDelete other<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<leader>xa', ':BDelete all<CR>', { noremap = true, silent = true })
 
