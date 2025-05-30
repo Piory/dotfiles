6ccf7caa41570e9a11fa8070c6872849bd514d7d
Piory <aiueo8102@gmail.com>
2025-05-30 15:37:10 +0900

    nvim に sphamba/smear-cursor.nvim を追加

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,5 +1,18 @@
 return {
   'gen740/SmoothCursor.nvim',
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = {                                -- Default  Range
+      stiffness = 0.8,                      -- 0.6      [0, 1]
+      trailing_stiffness = 0.5,             -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.6,          -- 0.4      [0, 1]
+      trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      distance_stop_animating = 0.5,        -- 0.1      > 0
+    },
+    config = function()
+      require('smear_cursor').setup()
+    end,
+  },
   'akinsho/bufferline.nvim',
   'lewis6991/gitsigns.nvim',
   'Mofiqul/dracula.nvim',
