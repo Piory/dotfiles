120a6983aabf2fc25d1794d0db3b392c09e7effd
Piory <aiueo8102@gmail.com>
2025-05-31 12:06:03 +0900

    nvim に simeji/winresizer を追加

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -4,6 +4,14 @@ return {
     event = 'VeryLazy',
   },
   {
+    'simeji/winresizer',
+    keys = { '<C-e>' },
+    init = function()
+      vim.g.winrisizer_vert_resize = 1
+      vim.g.winresizer_horiz_resize = 1
+    end,
+  },
+  {
     'mistricky/codesnap.nvim',
     build = 'make',
     keys = {
