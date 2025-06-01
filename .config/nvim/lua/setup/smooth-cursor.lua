80acf9307883cf4bbe1016a1d5b0d6ae26b71a78
Piory <aiueo8102@gmail.com>
2025-06-01 23:57:15 +0900

    SmoothCursor.nvim の設定変更

--- .config/nvim/lua/setup/smooth-cursor.lua
+++ .config/nvim/lua/setup/smooth-cursor.lua
@@ -1,27 +1,31 @@
-vim.api.nvim_set_hl(0, 'SmoothCursorCustom', { bg = nil, fg = '#42F57E', default = true })
+vim.api.nvim_set_hl(
+  0,
+  'SmoothCursorCustom',
+  { bg = nil, fg = '#42F57E', default = true }
+)
 require('smoothcursor').setup({
-	priority = 10,
-	fancy = {
-		enable = true,
-		head = { cursor = '', texthl = 'SmoothCursorCustom', linehl = nil },
-		body = {
-			{ cursor = '', texthl = 'SmoothCursorCustom' },
-			{ cursor = '', texthl = 'SmoothCursorCustom' },
-			{ cursor = '●', texthl = 'SmoothCursorCustom' },
-			{ cursor = '●', texthl = 'SmoothCursorCustom' },
-			{ cursor = '•', texthl = 'SmoothCursorCustom' },
-			{ cursor = '.', texthl = 'SmoothCursorCustom' },
-			{ cursor = '.', texthl = 'SmoothCursorCustom' },
-		},
-		tail = { cursor = nil, texthl = 'SmoothCursorCustom' },
-	},
-	disabled_filetypes = {
-		'TelescopePrompt',
-		'TelescopeResults',
-		'gitblame',
-		'css',
-		'noice',
-		'LspsagaHover',
-		'lazy',
-	},
+  priority = 10,
+  fancy = {
+    enable = true,
+    head = { cursor = '▷', texthl = 'SmoothCursorCustom', linehl = nil },
+    body = {
+      { cursor = '󰝥', texthl = 'SmoothCursorCustom' },
+      { cursor = '󰝥', texthl = 'SmoothCursorCustom' },
+      { cursor = '●', texthl = 'SmoothCursorCustom' },
+      { cursor = '●', texthl = 'SmoothCursorCustom' },
+      { cursor = '•', texthl = 'SmoothCursorCustom' },
+      { cursor = '.', texthl = 'SmoothCursorCustom' },
+      { cursor = '.', texthl = 'SmoothCursorCustom' },
+    },
+    tail = { cursor = nil, texthl = 'SmoothCursorCustom' },
+  },
+  disabled_filetypes = {
+    'TelescopePrompt',
+    'TelescopeResults',
+    'gitblame',
+    'css',
+    'noice',
+    'LspsagaHover',
+    'lazy',
+  },
 })
