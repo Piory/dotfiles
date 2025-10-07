49f668bbd1cc6e0d29aeaa4032e82cdb79d97303
Piory <aiueo8102@gmail.com>
2025-10-07 21:09:58 +0900

    🎨 style: キーマップに desc を付与して可読性向上

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,41 +4,40 @@ vim.g.mapleader = ' '                             -- <leader> を半角スペー
 keymap.set('i', 'jj', '<ESC>', { silent = true }) -- インサートモードを抜ける
 keymap.set('n', ';', ':', { silent = true })      -- シフトを押すことなく、;を入力する。割と便利
 
--- 検索ハイライトをすぐ消す（Esc×2で消える）
 vim.keymap.set('n', '<Esc><Esc>', function()
   vim.cmd('nohlsearch')
-  vim.fn.setreg('/', '') -- 検索パターン自体も空にしたい場合は有効化
-end, { silent = true })
+  -- 検索パターン自体も空にしたい場合は有効化
+  vim.fn.setreg('/', '')
+end, { silent = true, desc = '検索ハイライトを消す' })
 
-keymap.set('n', '<leader>w', ':w<CR>')                                                                                             -- 保存
-keymap.set('n', '<leader>q', ':q<CR>')                                                                                             -- 終了
-keymap.set('n', '<leader>c', ':nohlsearch<CR>')                                                                                    -- 検索のハイライトを消す
-keymap.set('n', '<leader>sv', '<C-w>v')                                                                                            -- ウィンドウを垂直方向に分割する
-keymap.set('n', '<leader>sh', '<C-w>s')                                                                                            -- ウィンドウを水平に分割する
-keymap.set('n', '<leader>se', '<C-w>=')                                                                                            -- ウィンドウの幅を等分にする
-keymap.set('n', '<leader>sx', ':close<CR>')                                                                                        -- 現在、カーソルがいるウィンドウを閉じる
-keymap.set('n', '<leader>sw', '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>')                                                  -- ウィンドウの幅を少し広げる
+keymap.set('n', '<leader>w', ':w<CR>', { silent = true, desc = '保存(:w)'})
+keymap.set('n', '<leader>q', ':q<CR>', { silent = true, desc= '終了(:q)'})
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'ウィンドウを垂直方向に分割する' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'ウィンドウを水平方向に分割する' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'ウィンドウの幅を等分にする' })
+keymap.set('n', '<leader>sx', ':close<CR>', { silent = true , desc = '現在、カーソルがいるウィンドウを閉じる' })
+keymap.set('n', '<leader>sw', '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>', { desc = 'ウィンドウの幅を少し広げる' })
 keymap.set('n', '<leader>sww',
-  '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>')                              -- ウィンドウの幅を大きく広げる
-keymap.set('n', '<leader>st', '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><')                                                  -- ウィンドウの幅を少し狭める
+  '<C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>><C-w>>', { desc = 'ウィンドウの幅を大きく広げる' })
+keymap.set('n', '<leader>st', '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><', { desc = 'ウィンドウの幅を少し狭める' })
 keymap.set('n', '<leader>stt',
-  '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><')                              -- ウィンドウの幅を大きく狭める
+  '<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><<C-w><', { desc = 'ウィンドウの幅を大きく狭める' })
 
 -- ターミナルモードのキーマップ
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('t', '<C-]>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- ウィンドウを移動する
-keymap.set('n', '<C-l>', '<C-w>l')
-keymap.set('n', '<C-h>', '<C-w>h')
-keymap.set('n', '<C-j>', '<C-w>j')
-keymap.set('n', '<C-k>', '<C-w>k')
+keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to the right window' })
+keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to the left window' })
+keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to the lower window' })
+keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to the upper window' })
 -- 単語を' " どちらかで囲う
-keymap.set('n', '<leader>aw', 'ciw""<Esc>P')
-keymap.set('n', '<leader>aW', "ciw''<Esc>P")
+keymap.set('n', '<leader>aw', 'ciw""<Esc>P', { desc = '単語を""で囲う' })
+keymap.set('n', '<leader>aW', "ciw''<Esc>P", { desc = "単語を''で囲う" })
 -- タブの移動
-keymap.set('n', 'gt', ':bnext<CR>')
-keymap.set('n', 'gT', ':bprev<CR>')
+keymap.set('n', 'gt', ':bnext<CR>', { silent = true, desc = '次のタブへ移動' })
+keymap.set('n', 'gT', ':bprev<CR>', { silent = true, desc = '前のタブへ移動' })
 
 -- LSP handlers
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
