c71180507896937f1a00bab7c6bc7adacffbc30b
Piory <aiueo8102@gmail.com>
2025-10-09 16:52:33 +0900

    ✨ feat: カラースキームを必要時に読み込むよう遅延化

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,6 @@
 return {
-  {
-    'Mofiqul/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'kepano/flexoki-neovim',
-    name = 'flexoki',
-    lazy = false,
-    priority = 1000,
-  },
+  { 'Mofiqul/dracula.nvim', lazy = true },
+  { 'folke/tokyonight.nvim', lazy = true, opts = {} },
+  { 'rebelot/kanagawa.nvim', lazy = true },
+  { 'kepano/flexoki-neovim', name = 'flexoki', lazy = true },
 }
