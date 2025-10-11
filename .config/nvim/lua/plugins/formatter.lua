47e84f53d0b6a059bebf016fa24c41164089aa42
Piory <aiueo8102@gmail.com>
2025-10-11 09:43:58 +0900

    ♻️ refactor: conform.nvim のフォーマッター判定を条件関数に整理

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -7,16 +7,17 @@ return {
       'mason-org/mason.nvim', -- Mason と統合するために必要
       'jayp0521/mason-null-ls.nvim', -- Mason と none-ls の統合
     },
-    config = function()
-      require('mason-null-ls').setup({
-        ensure_installed = {
-          'prettier',
-          'stylua',
-          'sqls',
-          'biome',
-        },
-        automatic_installation = true,
-      })
+    opts = {
+      ensure_installed = {
+        'prettier',
+        'stylua',
+        'sqls',
+        'biome',
+      },
+      automatic_installation = true,
+    },
+    config = function(_, opts)
+      require('mason-null-ls').setup(opts)
     end,
   },
   {
@@ -25,18 +26,47 @@ return {
       'BufRead',
       'BufNewFile',
     },
-    config = function()
-      local js_formatters = { 'prettier', 'prettierd', 'biome-check', stop_after_first = true }
-      require('conform').setup({
+    opts = function()
+      local js_formatters = { 'prettierd', 'prettier', 'biome-check', stop_after_first = true }
+      local biome_condition = function()
+        local buf = vim.api.nvim_get_current_buf()
+        local fname = vim.api.nvim_buf_get_name(buf)
+        local found = vim.fs.find({
+          'biome.json',
+          'biome.config.js',
+          'biome.config.cjs',
+        }, { upward = true, path = fname })[1]
+        return found ~= nil
+      end
+      local prettier_condition = function()
+        local buf = vim.api.nvim_get_current_buf()
+        local fname = vim.api.nvim_buf_get_name(buf)
+        local found = vim.fs.find({
+          'prettier.config.js',
+          'prettier.config.cjs',
+          '.prettierrc',
+          '.prettierrc.json',
+          '.prettierrc.json5',
+          '.prettierrc.yaml',
+          '.prettierrc.yml',
+          '.prettierrc.js',
+          '.prettierrc.cjs',
+        }, { upward = true, path = fname })[1]
+        return found ~= nil
+      end
+      return {
         formatters = {
-          biome_check = {
-            require_cwd = true,
-          },
           prettier = {
             require_cwd = true,
+            condition = prettier_condition,
           },
           prettierd = {
             require_cwd = true,
+            condition = prettier_condition,
+          },
+          ['biome-check'] = {
+            require_cwd = true,
+            condition = biome_condition,
           },
           gofmt = {
             require_cwd = true,
@@ -61,7 +91,10 @@ return {
           timeout_ms = 1000,
           lsp_fallback = true,
         },
-      })
+      }
+    end,
+    config = function(_, opts)
+      require('conform').setup(opts)
     end,
   },
 }
