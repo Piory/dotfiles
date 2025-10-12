7da6de4c64bedb22849ea5d9360002dbb8c1d6e5
Piory <aiueo8102@gmail.com>
2025-10-12 16:29:34 +0900

    🔨 chore: installスクリプトのログ表示をリデザイン

--- install.sh
+++ install.sh
@@ -8,25 +8,38 @@ CURRENT_DIRECTORY=$(pwd)
 
 # add submodule
 git submodule update --init --recursive
-echo 'Start install neovim!!!'
+echo 'Start setup dotfiles!!!'
 
-echo ''
-echo '######################################'
-echo '##       Setup neovim config        ##'
-echo '######################################'
-# Prezto
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                          Prezto                          │'
+echo '╰──────────────────────────────────────────────────────────╯'
 setopt EXTENDED_GLOB
 for rcfile in $CURRENT_DIRECTORY/.zprezto/runcoms/^README.md(.N); do
   echo "Create symbolic link ["${ZDOTDIR:-$HOME}/.${rcfile:t}" -> $rcfile]]"
   ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                    IntelliJ IDEA 関連                    │'
+echo '╰──────────────────────────────────────────────────────────╯'
 echo "Create symbolic link [$CONFIG_PATH/.ideavimrc -> $HOME]"
 ln -fs $CURRENT_DIRECTORY/.ideavimrc $HOME
+
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                          Neovim                          │'
+echo '╰──────────────────────────────────────────────────────────╯'
 echo "Create symbolic link [$CONFIG_PATH/nvim -> $CURRENT_DIRECTORY/.config/nvim]"
-ln -fs $CURRENT_DIRECTORY/.config/git $CONFIG_PATH
-echo "Create symbolic link [$CONFIG_PATH/git -> $CURRENT_DIRECTORY/.config/git]"
 ln -fs $CURRENT_DIRECTORY/.config/nvim $CONFIG_PATH
+
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                       Git Config                         │'
+echo '╰──────────────────────────────────────────────────────────╯'
+echo "Create symbolic link [$CONFIG_PATH/git -> $CURRENT_DIRECTORY/.config/git]"
+ln -fs $CURRENT_DIRECTORY/.config/git $CONFIG_PATH
+
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                          Other                           │'
+echo '╰──────────────────────────────────────────────────────────╯'
 dotfiles=(bin .zprezto .zshrc .zpreztorc .zprofile .p10k.zsh .tmux.conf .tmux .hyper.js .Brewfile .claude)
 for file in $dotfiles; do
   echo "Create symbolic link [$HOME/$file -> $CURRENT_DIRECTORY/$file]"
@@ -89,10 +102,9 @@ fi
 source $CURRENT_DIRECTORY/.zshrc
 source $CURRENT_DIRECTORY/.zpreztorc
 
-echo ''
-echo '######################################'
-echo '##          Install nodenv          ##'
-echo '######################################'
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                      Install nodenv                      │'
+echo '╰──────────────────────────────────────────────────────────╯'
 if ! builtin command -v nodenv > /dev/null; then
   echo 'Install nodenv'
   yes | anyenv install nodenv
@@ -100,10 +112,9 @@ else
   echo 'Installed nodenv'
 fi
 
-echo ''
-echo '######################################'
-echo '##           Install node           ##'
-echo '######################################'
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                       Install node                       │'
+echo '╰──────────────────────────────────────────────────────────╯'
 if ! builtin command -v node > /dev/null; then
   echo "Install node version is $NODE_VERSION"
   yes | nodenv install $NODE_VERSION
@@ -113,11 +124,12 @@ else
   echo "Installed node (version: $(node -v))"
 fi
 
-echo ''
-echo '######################################'
-echo '##        Install ClaudeCode        ##'
-echo '######################################'
 
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                    Install ClaudeCode                    │'
+echo '╰──────────────────────────────────────────────────────────╯'
 npm install -g @anthropic-ai/claude-code
 
-echo 'Neovim install completed!!!'
+echo '╭──────────────────────────────────────────────────────────╮'
+echo '│                     Finish setup!!!                      │'
+echo '╰──────────────────────────────────────────────────────────╯'
